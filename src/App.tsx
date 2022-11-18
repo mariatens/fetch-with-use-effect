3f3f037fed0e988ca5bbd9ba8109a7d6419410f0
Maria <mtens17@gmail.com>
2022-11-18 18:11:57 +0000

    Create interface, check fetching is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,45 @@
 import { useEffect, useState } from "react";
 
-// this interface is inside results. I need to get into results first
-interface Pokemon {
-  name:string;
-  url:string;
+interface Film {
+    id: string;
+    title: string;
+    original_title: string;
+    original_title_romanised: string;
+    image: string;
+    movie_banner: string;
+    description: string;
+    director: string;
+    producer: string;
+    release_date: number;
+    running_time: number;
+    rt_score: number;
+    people: string[];
+    species: string[];
+    locations: string[];
+    vehicles: string[];
+    url: string;
 }
 
 function App() {
 
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [films, setFilms] = useState<any[]>([]);
 
   useEffect(() => {
-    const fetchPokemon = async () => {
+    const fetchFilm = async () => {
       const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=151'"
+        "https://ghibliapi.herokuapp.com/films/"
       );
       const jsonBody= await response.json();
-      setPokemons(jsonBody.results);
-      console.log(jsonBody.results)
+      setFilms(jsonBody);
     };
 
-    fetchPokemon();
+    fetchFilm();
   }, []);
 
   return (
     <>
-      <h1>Pokemon App</h1>
-      <p>{pokemons.map((eachPokemon)=> <li key = {eachPokemon.url}>{eachPokemon.name}</li>
+      <h1>Film App</h1>
+      <p>{films.map((eachFilm: any)=> <li key = {eachFilm.title}>{eachFilm.title}</li>
       )}</p>
     </>
   );
